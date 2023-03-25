887459b149f9bb136beb6ae2f59554922fe00c45
Samuel Wilcox <samuelwilcox@outlook.com>
2023-03-25 23:19:17 +0000

    move evaluate funcs to be a part of the fields implementation

--- fields.go
+++ fields.go
@@ -1,28 +1,29 @@
 package goscanql
 
-import "fmt"
-
-func newFields() *fields {
-	return &fields{
-		orderedFieldNames: make([]string, 0),
-		references:        make(map[string]interface{}),
-		children:          make(map[string]*fields),
-	}
-}
+import (
+	"fmt"
+	"reflect"
+)
 
 type fields struct {
+	objRef            interface{}
 	orderedFieldNames []string
 	references        map[string]interface{}
 	children          map[string]*fields
 }
 
-func (f *fields) addChild(name string) *fields {
+func (f *fields) addNewChild(name string, obj interface{}) error {
 	if _, ok := f.children[name]; ok {
 		panic(fmt.Errorf("child with same name (\"%s\") already exists", name))
 	}
 
-	f.children[name] = newFields()
-	return f.children[name]
+	child, err := newFields(obj)
+	if err != nil {
+		return err
+	}
+
+	f.children[name] = child
+	return nil
 }
 
 func (f *fields) addField(name string, value interface{}) {
@@ -80,4 +81,128 @@ func (f *fields) crawlReferencesWithPrefix(prefix string, fn func(key string, va
 		childPrefix := fmt.Sprintf("%s%s", prefix, name)
 		child.crawlReferencesWithPrefix(childPrefix, fn)
 	}
+}
+
+func newFields(obj interface{}) (*fields, error) {
+
+	fields := &fields{
+		objRef:            obj,
+		orderedFieldNames: make([]string, 0),
+		references:        make(map[string]interface{}),
+		children:          make(map[string]*fields),
+	}
+
+	err := initialiseFields(obj, fields)
+	if err != nil {
+		return nil, err
+	}
+
+	return fields, nil
+}
+
+func initialiseFields(obj interface{}, fields *fields) error {
+
+	rv := instantiateAndReturn(obj)
+	t := rv.Type()
+
+	// if type is slice, add 1 element to it to store values
+	if rv.Kind() == reflect.Slice {
+
+		// get slice type, e.g. []*Example has a slice type of *Example
+		sliceType := reflect.TypeOf(rv.Interface()).Elem()
+
+		// create new element of sliceType
+		element := reflect.New(sliceType).Elem()
+
+		// instantiate element's root value
+		elementValue := instantiateAndReturn(element.Addr().Interface())
+
+		// append new element to slice
+		rv.Set(reflect.Append(rv, element))
+
+		// add child and evaluate it
+		return initialiseFields(elementValue.Addr().Interface(), fields)
+	}
+
+	// extract expected fields
+	for i := 0; i < t.NumField(); i++ {
+
+		fieldType := t.Field(i)
+		fieldValue := rv.Field(i)
+
+		fieldName, ok := fieldType.Tag.Lookup(scanqlTag)
+
+		// skip if field doesn't have scanql tag
+		if !ok {
+			continue
+		}
+
+		fieldValueRoot := instantiateAndReturn(fieldValue.Addr().Interface())
+
+		// if nested struct or slice
+		if fieldValueRoot.Kind() == reflect.Struct || fieldValueRoot.Kind() == reflect.Slice {
+
+			// evaluate with pointer to new instance
+			err := fields.addNewChild(fieldName, fieldValueRoot.Addr().Interface())
+			if err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		// add field to map
+		fields.addField(fieldName, rv.Field(i).Addr().Interface())
+	}
+
+	return nil
+}
+
+// instantiateAndReturn will take any value and instantiate it with the equivalent Zero
+// value for that type, e.g. 0 for int or an empty struct for a struct. It will then return
+// that value as a reflect.Value.
+//
+// If the type is a pointer (at any level, e.g. *int or ****int) the function will traverse
+// to the very root of the pointers (in this case to the int) and instantiate and return
+// that. The original pointers will be set to point to this new value also.
+//
+// Note, if the pointer is uninitialised, to keep a reference to it you will need to pass
+// it in as a pointer, for example:
+//
+//	var i *int
+//
+// would need to be passed in as
+//
+//	instantiateAndReturn(&i)
+//
+// as the default value would be nil, and therefore is not addressable. However if the pointer
+// is initialised e.g. ip in this example:
+//
+//	var i int
+//	ip := &i
+//
+// then that can be passed in directly:
+//
+//	instantiateAndReturn(ip)
+func instantiateAndReturn[T any](t T) reflect.Value {
+	fmt.Println(reflect.TypeOf(t))
+	return instantiateValue(reflect.ValueOf(t).Elem())
+}
+
+func instantiateValue(val reflect.Value) reflect.Value {
+
+	// get value of i (must pass in as pointer), see:
+	// https://stackoverflow.com/questions/34145072/can-you-initialise-a-pointer-variable-with-golang-reflect
+
+	// if we are not at root
+	if val.Kind() == reflect.Pointer {
+
+		// instantiate current value
+		val.Set(reflect.New(val.Type().Elem()))
+
+		// crawl further
+		return instantiateValue(val.Elem())
+	}
+
+	return val
 }
--- goscanql.go
+++ goscanql.go
@@ -3,82 +3,11 @@ package goscanql
 import (
 	"database/sql"
 	"fmt"
-	"reflect"
 )
 
 const (
 	scanqlTag = "goscanql"
 )
-
-func evaluate(obj interface{}) (*fields, error) {
-
-	expectedFields := newFields()
-
-	err := evaluateStruct(obj, expectedFields)
-	if err != nil {
-		return nil, err
-	}
-
-	return expectedFields, nil
-}
-
-func evaluateStruct(obj interface{}, fields *fields) error {
-
-	rv := instantiateAndReturn(obj)
-	t := rv.Type()
-
-	// if type is slice, add 1 element to it to store values
-	if rv.Kind() == reflect.Slice {
-
-		// get slice type, e.g. []*Example has a slice type of *Example
-		sliceType := reflect.TypeOf(rv.Interface()).Elem()
-
-		// create new element of sliceType
-		element := reflect.New(sliceType).Elem()
-
-		// instantiate element's root value
-		elementValue := instantiateAndReturn(element.Addr().Interface())
-
-		// append new element to slice
-		rv.Set(reflect.Append(rv, element))
-
-		// evaluate element
-		return evaluateStruct(elementValue.Addr().Interface(), fields)
-	}
-
-	// extract expected fields
-	for i := 0; i < t.NumField(); i++ {
-
-		fieldType := t.Field(i)
-		fieldValue := rv.Field(i)
-
-		fieldName, ok := fieldType.Tag.Lookup(scanqlTag)
-
-		// skip if field doesn't have scanql tag
-		if !ok {
-			continue
-		}
-
-		fieldValueRoot := instantiateAndReturn(fieldValue.Addr().Interface())
-
-		// if nested struct or slice
-		if fieldValueRoot.Kind() == reflect.Struct || fieldValueRoot.Kind() == reflect.Slice {
-
-			// evaluate with pointer to new instance
-			err := evaluateStruct(fieldValueRoot.Addr().Interface(), fields.addChild(fieldName))
-			if err != nil {
-				return err
-			}
-
-			continue
-		}
-
-		// add field to map
-		fields.addField(fieldName, rv.Field(i).Addr().Interface())
-	}
-
-	return nil
-}
 
 func mapFieldsToColumns(cols []string, fields map[string]interface{}) []interface{} {
 
@@ -110,7 +39,7 @@ func scanRows[T any](rows *sql.Rows) ([]T, error) {
 
 		entry := new(T)
 
-		fields, err := evaluate(entry)
+		fields, err := newFields(entry)
 		if err != nil {
 			return nil, err
 		}
@@ -124,55 +53,6 @@ func scanRows[T any](rows *sql.Rows) ([]T, error) {
 	}
 
 	return result, nil
-}
-
-// instantiateAndReturn will take any value and instantiate it with the equivalent Zero
-// value for that type, e.g. 0 for int or an empty struct for a struct. It will then return
-// that value as a reflect.Value.
-//
-// If the type is a pointer (at any level, e.g. *int or ****int) the function will traverse
-// to the very root of the pointers (in this case to the int) and instantiate and return
-// that. The original pointers will be set to point to this new value also.
-//
-// Note, if the pointer is uninitialised, to keep a reference to it you will need to pass
-// it in as a pointer, for example:
-//
-//	var i *int
-//
-// would need to be passed in as
-//
-//	instantiateAndReturn(&i)
-//
-// as the default value would be nil, and therefore is not addressable. However if the pointer
-// is initialised e.g. ip in this example:
-//
-//	var i int
-//	ip := &i
-//
-// then that can be passed in directly:
-//
-//	instantiateAndReturn(ip)
-func instantiateAndReturn[T any](t T) reflect.Value {
-	fmt.Println(reflect.TypeOf(t))
-	return instantiateValue(reflect.ValueOf(t).Elem())
-}
-
-func instantiateValue(val reflect.Value) reflect.Value {
-
-	// get value of i (must pass in as pointer), see:
-	// https://stackoverflow.com/questions/34145072/can-you-initialise-a-pointer-variable-with-golang-reflect
-
-	// if we are not at root
-	if val.Kind() == reflect.Pointer {
-
-		// instantiate current value
-		val.Set(reflect.New(val.Type().Elem()))
-
-		// crawl further
-		return instantiateValue(val.Elem())
-	}
-
-	return val
 }
 
 // TODO this func will group the rows into the correct struct arrays and fields

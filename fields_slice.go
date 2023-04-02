a15d99450d3c29173510be0f3381e7a85670e6d9
Samuel Wilcox <samuelwilcox@outlook.com>
2023-04-02 17:33:53 +0100

    move fieldsSlice to it's own file and rename fieldsSlice test file

---
+++ fields_slice.go
@@ -0,0 +1,67 @@
+package goscanql
+
+import "reflect"
+
+// fieldsSlice maintains a record of a slice of entities along with the
+// fields representations of those entities to facilitate fields merging.
+type fieldsSlice struct {
+
+	// sliceRef is the reference of the slice containing the entities as their
+	// own type.
+	sliceRef interface{}
+
+	// fields represents the entities contained in sliceRef but as fields (to
+	// facilitate hash lookups).
+	fields []*fields
+}
+
+// append will add the provided fields (and the entity it represents) into
+// the fieldsSlice.
+func (fs *fieldsSlice) append(f *fields) {
+
+	parent := reflect.ValueOf(fs.sliceRef).Elem()
+	fParent := reflect.ValueOf(f.slice.sliceRef).Elem()
+
+	parent.Set(reflect.Append(parent, fParent.Index(0)))
+	fs.fields = append(fs.fields, f)
+}
+
+// getExisting returns the existing *fields entity that is contained within
+// the fieldsSlice by looking up the provided fields hash.
+//
+// nil is returned when the fields entity doesn't already exist.
+func (fs *fieldsSlice) getExisting(f *fields) *fields {
+
+	fHash := f.getHash()
+
+	for _, existing := range fs.fields {
+		if existing.getHash() == fHash {
+			return existing
+		}
+	}
+
+	return nil
+}
+
+// empty will set the slice to be an empty slice (removing any contained elements)
+func (fc *fieldsSlice) empty() {
+
+	if fc.sliceRef == nil {
+		return
+	}
+
+	t := reflect.TypeOf(fc.sliceRef).Elem()
+	rv := reflect.ValueOf(fc.sliceRef).Elem()
+
+	rv.Set(reflect.MakeSlice(t, 0, 0))
+}
+
+// newFieldsSlice is a fieldsSlice constructor
+func newFieldsSlice(container interface{}, f *fields) *fieldsSlice {
+	return &fieldsSlice{
+		sliceRef: container,
+		fields: []*fields{
+			f,
+		},
+	}
+}

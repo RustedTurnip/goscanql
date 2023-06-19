45888d2b9ddec1decc72aa0e423eecd3ef6b7d95
Samuel Wilcox <samuelwilcox@outlook.com>
2023-06-19 08:39:57 +0100

    improve record_map.go's documentation

--- record_map.go
+++ record_map.go
@@ -4,33 +4,49 @@ import (
 	"reflect"
 )
 
-// recordList TODO map[fieldHash]record
+// recordList is a type of map[string]record, where the key is the unique hash of the entity it
+// represents (the hash is taken from the entity's initialised fields) and the value is of type
+// record (used to locate a particular entry in a one-to-many relationship).
+//
+// In short, the recordList type is used to bind an entity hash to a record for later look-ups
+// when performing entity merges.
 type recordList map[string]record
 
+// record represents a single child entity of a one-to-many relationship. This is used to look up
+// a value (from the parent slice) that a fields needs to be merged with (by matching the stored
+// hash of the record, with the hash of a fields).
 type record struct {
+
+	// index is the position/index in the slice that the entity the record represents is located
+	// at.
 	index int
 
-	// map[fieldName]recordList
+	// otmChildren is the list of child one-to--many relationships that the entity this record
+	// represents has. The type can be thought of as: map[fieldName]recordList.
 	otmChildren map[string]recordList
 }
 
+// recordMap maintains a slice of the root type that goscanql is called with, and then the entry
+// recordList for this (the recordList representing root entities rather than child one-to-manys).
+// recordMap can be used to merge a fields into the existing entities.
 type recordMap[T any] struct {
-	// entries represents the
+
+	// entries represents the list of currently parsed entities of type T.
 	entries []T
 
-	// hashTable (TODO rename) holds the unique value hash for each entry, and is linked to
-	// the entry through the record.index.. TODO
+	// hashTable represents the hashes and record information of the entities stored in the entries slice
+	// of recordMap. This is used for entity matching during a merge to ensure that new data is added in
+	// the right place rather than adding duplicate values.
 	hashTable recordList
 }
 
-// insert TODO
+// insert will add the provided value of rv to the provided slice as a new value.
 func (rl recordList) insert(entry *fields, rv *reflect.Value, slice interface{}) {
 
 	// only perform append if the provided value isn't nil (suggesting that the insert is at
-	// the point in the fields where it needs to be appended) - children after this point don't
-	// need to be appended because they already exist in obj
+	// the point in the fields where it needs to be appended). Children after this point don't
+	// need to be appended because they already exist in obj.
 	if rv != nil { // TODO maybe need to check if obj is nil?
-
 		srv := reflect.ValueOf(slice).Elem()
 		srv.Set(reflect.Append(srv, *rv))
 	}
@@ -49,7 +65,10 @@ func (rl recordList) insert(entry *fields, rv *reflect.Value, slice interface{})
 	rl[entry.getHash()] = r
 }
 
-// merge TODO
+// merge will recursively search the provided fields against the stored records to determine
+// how the value represented by fields should be combined into the existing entries. Where a
+// one-to-many relationship is found where no child matches the hash of the fields, this will
+// be added as a new value in the one-to-many slice.
 func (rl recordList) merge(entry *fields, rv *reflect.Value, slice interface{}) {
 
 	// TODO should this be in here, it feels like isNil should be more private to fields
@@ -74,13 +93,16 @@ func (rl recordList) merge(entry *fields, rv *reflect.Value, slice interface{}) 
 	}
 }
 
-// merge TODO
+// merge will apply the provided fields to the existing entities maintained by recordMap, using
+// fields hash values to determine where the data already exists, or where it should be added
+// as new.
 func (rm *recordMap[T]) merge(entry *fields) {
 	rv := reflect.ValueOf(entry.obj).Elem()
 	rm.hashTable.merge(entry, &rv, &rm.entries)
 }
 
-// newRecordMap TODO
+// newRecordMap is the constructor for record map, and will return an instantiated recordMap
+// based on the provided type T.
 func newRecordMap[T any]() *recordMap[T] {
 	return &recordMap[T]{
 		entries:   make([]T, 0),
@@ -88,12 +110,16 @@ func newRecordMap[T any]() *recordMap[T] {
 	}
 }
 
+// fieldByTag will look up a field of the provided value (v) by the field's tag value (where
+// the field is tagged with goscanql). If no field matches the provided tag, then nil is
+// returned.
 func fieldByTag(tag string, v reflect.Value) *reflect.Value {
 
 	tv := v.Type()
 
 	for i := 0; i < v.NumField(); i++ {
 
+		// TODO use variable instead of hard coded "goscanql" string
 		if tv.Field(i).Tag.Get("goscanql") != tag {
 			continue
 		}

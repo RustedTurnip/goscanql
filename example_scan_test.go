36075b4777f3542f4be816a04c8db71b5cd52186
Samuel Wilcox <samuelwilcox@outlook.com>
2023-06-20 21:58:36 +0100

    extend example_scan_test.go to include primitive and nested one-to-manys

--- example_scan_test.go
+++ example_scan_test.go
@@ -12,11 +12,15 @@ const (
 			user.id AS id,
 			user.name AS name,
 			user.date_of_birth AS date_of_birth,
-			vehicle.medium AS vehicle_medium,
+			user_alias.alias AS alias,
 			vehicle.type AS vehicle_type,
 			vehicle.colour AS vehicle_colour,
-			vehicle.noise AS vehicle_noise
-		FROM user LEFT JOIN vehicle ON user.id = vehicle.user_id;`
+			vehicle.noise AS vehicle_noise,
+			vehicle_medium.name AS vehicle_medium_name
+		FROM user
+		LEFT JOIN user_alias ON user.id=user_alias.user_id
+		LEFT JOIN vehicle ON user.id = vehicle.user_id
+        LEFT JOIN vehicle_medium ON vehicle.medium_id=vehicle_medium.id;`
 )
 
 // User represents an example user struct that you might want to parse data into
@@ -24,14 +28,20 @@ type User struct {
 	Id       int       `goscanql:"id"`
 	Name     string    `goscanql:"name"`
 	Vehicles []Vehicle `goscanql:"vehicle"`
+	Aliases  []string  `goscanql:"alias"`
 }
 
 // Vehicle represents an example vehicle struct that you might want to parse data into
 type Vehicle struct {
-	Medium string `goscanql:"medium"`
-	Type   string `goscanql:"type"`
-	Colour string `goscanql:"colour"`
-	Noise  string `goscanql:"noise"`
+	Type    string          `goscanql:"type"`
+	Colour  string          `goscanql:"colour"`
+	Noise   string          `goscanql:"noise"`
+	Mediums []VehicleMedium `goscanql:"medium"`
+}
+
+// VehicleMedium represents the "medium" upon which a vehicle operates
+type VehicleMedium struct {
+	Name string `goscanql:"name"`
 }
 
 func ExampleRowsToStructs() {
@@ -42,15 +52,17 @@ func ExampleRowsToStructs() {
 		panic(err)
 	}
 
-	columns := []string{"id", "name", "vehicle_medium", "vehicle_type", "vehicle_colour", "vehicle_noise"}
+	columns := []string{"id", "name", "alias", "vehicle_type", "vehicle_colour", "vehicle_noise", "vehicle_medium_name"}
 	inputRows := sqlmock.NewRows(columns)
 
-	inputRows.AddRow(1, "Stirling Archer", "land", "car", "black", "brum")
-	inputRows.AddRow(2, "Cheryl Tunt", "air", "aeroplane", "white", "whoosh")
-	inputRows.AddRow(3, "Algernop Krieger", "land", "van", "blue", "brum")
-	inputRows.AddRow(3, "Algernop Krieger", "sea", "submarine", "black", "...")
-	inputRows.AddRow(4, "Barry Dylan", "space", "spaceship", "grey", "RRRRRRRRRRRRRRRRRRGGHHHH")
-	inputRows.AddRow(4, "Barry Dylan", "land", "motorbike", "black", "vroom")
+	inputRows.AddRow(1, "Stirling Archer", "", "car", "black", "brum", "land")
+	inputRows.AddRow(2, "Cheryl Tunt", "Chrystal", "aeroplane", "white", "whoosh", "air")
+	inputRows.AddRow(2, "Cheryl Tunt", "Charlene", "aeroplane", "white", "whoosh", "air")
+	inputRows.AddRow(3, "Algernop Krieger", "", "van", "blue", "brum", "land")
+	inputRows.AddRow(3, "Algernop Krieger", "", "submarine", "black", "...", "sea")
+	inputRows.AddRow(3, "Algernop Krieger", "", "submarine", "black", "...", "swimming pool")
+	inputRows.AddRow(4, "Barry Dylan", "", "spaceship", "grey", "RRRRRRRRRRRRRRRRRRGGHHHH", "space")
+	inputRows.AddRow(4, "Barry Dylan", "", "motorbike", "black", "vroom", "land")
 
 	mock.ExpectQuery(exampleQuery).WillReturnRows(inputRows)
 

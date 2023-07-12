c9a169043f29c922ef90392325cf520eae948b4d
Samuel Wilcox <samuel@sams-space.local>
2023-07-12 07:19:38 +0100

    add documentation to scanner.go and use sql.Scanner for interface

--- scanner.go
+++ scanner.go
@@ -1,13 +1,22 @@
 package goscanql
 
-import "reflect"
+import (
+	"database/sql"
+	"reflect"
+)
 
+// Scanner represents an interface to provide custom scanning logic to a field for
+// goscanql. This can be used to parse data from an sql column in a non-default
+// way, for example parsing a string into a struct instead of a string, or to
+// provide a way to scan data into a type that is otherwise unsupported like an
+// array or a multi-dimensional slice.
 type Scanner interface {
-	Scan(interface{}) error
+	sql.Scanner
 	GetID() string
 }
 
-// TODO comment this
+// implementsScanner evaluates the provided type and returns true if it implements
+// the Scanner interface, or false otherwise.
 func implementsScanner(t reflect.Type) bool {
 	return t.Implements(reflect.TypeOf((*Scanner)(nil)).Elem())
 }
